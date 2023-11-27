7b2ba1a41165378e8bece0784a1c89e3e1d8affc
sang0913 <74355128+sang0913@users.noreply.github.com>
2023-11-27 21:31:04 +0700

    no message

---
+++ SwiftUI_Components/Animation/Youtube/SkeletonView-ShimmerView.swift
@@ -0,0 +1,55 @@
+//
+//  SkeletonView.swift
+//  SwiftUI_Components
+//
+//  Created by Em bé cute on 11/27/23.
+//
+import SwiftUI
+
+private struct SkeletonViewShimmerView: View {
+    @State private var shimmering = false
+    
+    let streamSnow = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+    let streamGray = #colorLiteral(red: 0.5741485357, green: 0.5741624236, blue: 0.574154973, alpha: 1)
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                ForEach(0 ..< 8) { item in
+                    HStack {
+                        Image(systemName: "bell.fill")
+                            .font(.system(size: 54))
+                        
+                        VStack(alignment: .leading) {
+                            Text("TplaceholderTpla")
+                            Text("So cool")
+                        }
+                        .font(.title)
+                    }
+                    .foregroundStyle(
+                        .linearGradient(
+                            colors: [Color(streamSnow), Color(streamGray)],
+                            startPoint: .leading,
+                            endPoint: shimmering ? .trailing : .leading
+                        )
+                    )
+                    .redacted(reason: .placeholder)
+                }
+                .padding()
+                .offset(x: -50, y: 100)
+                .animation(.easeOut(duration: 1.5).repeatForever(autoreverses: false), value: shimmering)
+                .onAppear {
+                    shimmering.toggle()
+                }
+            }
+            .navigationTitle("Animated PlaceHolder")
+        }
+    }
+}
+
+struct SkeletonViewShimmerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkeletonViewShimmerView()
+            .preferredColorScheme(.dark)
+    }
+}
