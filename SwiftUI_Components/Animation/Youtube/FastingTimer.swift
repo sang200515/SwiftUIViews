b1fff75d1d8fe5d88369471a5f372bc707c92363
sang0913 <74355128+sang0913@users.noreply.github.com>
2023-11-28 20:30:28 +0700

    no message

---
+++ SwiftUI_Components/Animation/Youtube/FastingTimer.swift
@@ -0,0 +1,310 @@
+//
+//  FastingTimer.swift
+//  SwiftUI_Components
+//
+//  Created by Em bé cute on 11/27/23.
+//
+
+import SwiftUI
+
+private struct FastingTimer: View {
+    @StateObject var fastingManager = FastingManager()
+    
+    var title: String {
+        switch fastingManager.fastingState {
+        case .notStarted:
+            return "Let's get started!"
+        case .fasting:
+            return "You are now fasting"
+        case .feeding:
+            return "You are now feeding"
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            // MARK: Background
+            
+            Color(#colorLiteral(red: 0.05024140328, green: 0.006751002744, blue: 0.08163713664, alpha: 1))
+                .ignoresSafeArea()
+            
+            content
+        }
+    }
+    
+    var content: some View {
+        ZStack {
+            VStack(spacing: 40) {
+                // MARK: Title
+                
+                Text(title)
+                    .font(.headline)
+                    .foregroundColor(Color(#colorLiteral(red: 0.3843137255, green: 0.5176470588, blue: 1, alpha: 1)))
+                
+                // MARK: Fasting Plan
+                
+                Text(fastingManager.fastingPlan.rawValue)
+                    .fontWeight(.semibold)
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 8)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+                
+                Spacer()
+            }
+            .padding()
+            
+            VStack(spacing: 40) {
+                // MARK: Progress Ring
+                
+                ProgressRing()
+                    .environmentObject(fastingManager)
+                
+                HStack(spacing: 60) {
+                    // MARK: Start Time
+                    
+                    VStack(spacing: 5) {
+                        Text(fastingManager.fastingState == .notStarted ? "Start" : "Started")
+                            .opacity(0.7)
+                        
+                        Text(fastingManager.startTime, format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                    
+                    // MARK: End Time
+                    
+                    VStack(spacing: 5) {
+                        Text(fastingManager.fastingState == .notStarted ? "End" : "Ends")
+                            .opacity(0.7)
+                        
+                        Text(fastingManager.endTime, format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                }
+                
+                // MARK: Button
+                
+                Button {
+                    fastingManager.toggleFastingState()
+                } label: {
+                    Text(fastingManager.fastingState == .fasting ? "End fast" : "Start fasting")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 8)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                }
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct FastingTimer_Previews: PreviewProvider {
+    static var previews: some View {
+        FastingTimer()
+    }
+}
+//
+//  FastingManager.swift
+//  FastingTimer
+//
+//  Created by Dara To on 2022-01-16.
+//
+
+import Foundation
+
+private enum FastingState {
+    case notStarted
+    case fasting
+    case feeding
+}
+
+private enum FastingPlan: String {
+    case beginner = "12:12"
+    case intermediate = "16:8"
+    case advanced = "20:4"
+    
+    var fastingPeriod: Double {
+        switch self {
+        case .beginner:
+            return 12
+        case .intermediate:
+            return 16
+        case .advanced:
+            return 20
+        }
+    }
+}
+
+private final class FastingManager: ObservableObject {
+    @Published private(set) var fastingState: FastingState = .notStarted
+    @Published private(set) var fastingPlan: FastingPlan = .intermediate
+    @Published private(set) var startTime: Date {
+        didSet {
+            print("startTime", startTime.formatted(.dateTime.month().day().hour().minute().second()))
+            
+            if fastingState == .fasting {
+                endTime = startTime.addingTimeInterval(fastingTime)
+            } else {
+                endTime = startTime.addingTimeInterval(feedingTime)
+            }
+        }
+    }
+    @Published private(set) var endTime: Date {
+        didSet {
+            print("endTime", endTime.formatted(.dateTime.month().day().hour().minute().second()))
+        }
+    }
+    @Published private(set) var elapsed: Bool = false
+    @Published private(set) var elapsedTime: Double = 0.0
+    @Published private(set) var progress: Double = 0.0
+    
+    var fastingTime: Double {
+        return fastingPlan.fastingPeriod * 60 * 60
+    }
+    var feedingTime: Double {
+        return (24 - fastingPlan.fastingPeriod) * 60 * 60
+    }
+    
+    init() {
+        let calendar = Calendar.current
+        
+//        var components = calendar.dateComponents([.year, .month, .day, .hour], from: Date())
+//        components.hour = 20
+//        print(components)
+//
+//        let scheduledTime = calendar.date(from: components) ?? Date.now
+//        print("scheduledTime", scheduledTime.formatted(.dateTime.month().day().hour().minute().second()))
+        
+        let components = DateComponents(hour: 20)
+
+        let scheduledTime = calendar.nextDate(after: .now, matching: components, matchingPolicy: .nextTime)!
+        print("scheduledTime", scheduledTime.formatted(.dateTime.month().day().hour().minute().second()))
+        
+        startTime = scheduledTime
+        endTime = scheduledTime.addingTimeInterval(FastingPlan.intermediate.fastingPeriod * 60 * 60)
+    }
+    
+    func toggleFastingState() {
+        fastingState = fastingState == .fasting ? .feeding : .fasting
+        startTime = Date()
+        elapsedTime = 0.0
+    }
+    
+    func track() {
+        guard fastingState != .notStarted else { return }
+        
+        print("now", Date().formatted(.dateTime.month().day().hour().minute().second()))
+        
+        if endTime >= Date() {
+            print("Not elapsed")
+            elapsed = false
+        } else {
+            print("Elapsed")
+            elapsed = true
+        }
+
+        elapsedTime += 1
+        print("elapsedTime", elapsedTime)
+        
+        let totalTime = fastingState == .fasting ? fastingTime : feedingTime
+        progress = (elapsedTime / totalTime * 100).rounded() / 100
+        print("progress", progress)
+    }
+}
+//
+//  ProgressRing.swift
+//  FastingTimer
+//
+//  Created by Dara To on 2022-01-16.
+//
+
+import SwiftUI
+
+private struct ProgressRing: View {
+    @EnvironmentObject var fastingManager: FastingManager
+
+    let timer = Timer
+        .publish(every: 1, on: .main, in: .common)
+        .autoconnect()
+    
+    var body: some View {
+        ZStack {
+            // MARK: Placeholder Ring
+            
+            Circle()
+                .stroke(lineWidth: 20)
+                .foregroundColor(.gray)
+                .opacity(0.1)
+            
+            // MARK: Colored Ring
+            
+            Circle()
+                .trim(from: 0.0, to: min(fastingManager.progress, 1.0))
+                .stroke(AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3843137255, green: 0.5176470588, blue: 1, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.4470588235, blue: 0.7137254902, alpha: 1)), Color(#colorLiteral(red: 0.8509803922, green: 0.6862745098, blue: 0.8509803922, alpha: 1)), Color(#colorLiteral(red: 0.5921568627, green: 0.8509803922, blue: 0.8823529412, alpha: 1)), Color(#colorLiteral(red: 0.3843137255, green: 0.5176470588, blue: 1, alpha: 1))]), center: .center), style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
+                .rotationEffect(Angle(degrees: 270))
+                .animation(.easeInOut(duration: 1.0), value: fastingManager.progress)
+            
+            VStack(spacing: 30) {
+                if fastingManager.fastingState == .notStarted {
+                    // MARK: Upcoming Fast
+                    
+                    VStack(spacing: 5) {
+                        Text("Upcoming fast")
+                            .opacity(0.7)
+                        
+                        Text("\(fastingManager.fastingPlan.fastingPeriod.formatted()) Hours")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                } else {
+                    // MARK: Elapsed Time
+                    
+                    VStack(spacing: 5) {
+                        Text("Elapsed time (\(fastingManager.progress.formatted(.percent)))")
+                            .opacity(0.7)
+                        
+                        Text(fastingManager.startTime, style: .timer)
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                    .padding(.top)
+                    
+                    // MARK: Remaining Time
+                    
+                    VStack(spacing: 5) {
+                        if !fastingManager.elapsed {
+                            Text("Remaining time (\((1 - fastingManager.progress).formatted(.percent)))")
+                                .opacity(0.7)
+                        } else {
+                            Text("Extra time")
+                                .opacity(0.7)
+                        }
+                        
+                        Text(fastingManager.endTime, style: .timer)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
+                }
+            }
+        }
+        .frame(width: 250, height: 250)
+        .padding()
+//        .onAppear {
+//            fastingManager.progress = 1
+//        }
+        .onReceive(timer) { _ in
+            fastingManager.track()
+        }
+    }
+}
+
+struct ProgressRing_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressRing()
+            .environmentObject(FastingManager())
+    }
+}
---
+++ SwiftUI_Components/Animation/Youtube/QuestionView.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionView.swift
+//  SwiftUI_Components
+//
+//  Created by Em bé cute on 11/27/23.
+//
+
+import Foundation
+//https://github.com/stephdiep/TriviaGameApp/tree/ca501be9e74fd782f7c988067693b1f8c275c35c
